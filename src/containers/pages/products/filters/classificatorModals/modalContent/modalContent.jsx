b29cacaeb74ba909223ba3c7209d980844aaf9aa
HaykMalkhasyan <hayko.malxasyan@mail.ru>
2020-10-30 19:31:25 +0400

    debug.log changes 4

--- src/containers/pages/products/filters/classificatorModals/modalContent/modalContent.jsx
+++ src/containers/pages/products/filters/classificatorModals/modalContent/modalContent.jsx
@@ -1,9 +1,15 @@
 import React, {useState} from 'react'
 import classes from './modalContent.module.css'
+import DeleteModal from "../../../../../../components/deleteModal/deleteModal";
+import Backdrop from "../../../../../../components/UI/backdrop/backdrop";
+import CustomCheckbox from "../../../../../../components/UI/input/customCheckbox/customCheckbox";
+import CustomSearch from "../../../../../../components/customSearch/customSearch";
+import CustomInput from "../../../../../../components/UI/input/customInput/customInput";
 import HeaderContent from "./header-content/header-content";
+import ModalActions from "./actions/actions";
 import FooterContent from "./footer-content/footer-content";
-import SectionContent from "./section-content/section-content";
-import DeleteContent from "./delete-content/delete-content";
+import BodyContent from "./body-content/body-content";
+import cookie from "../../../../../../services/cookies";
 
 const ModalContent = props => {
     const [error, setError] = useState(null);
@@ -13,15 +19,14 @@ const ModalContent = props => {
     };
 
     const deleteModalCloseHandler = () => {
-        props.deleteModalClose();
+        props.setGroupValues('delete', false);
+        props.setGroupValues('subgroup', null);
     };
 
     const deleteModalConfirmHandler = id => {
-        if (props.delete === "group") {
-            props.deleteAction("Group/Group", id);
-        } else if (props.delete === "subgroup") {
-            props.deleteAction("Group/SubGroup", id, props.catId);
-        }
+        props.deleteSubgroup(id);
+        props.setGroupValues('delete', false);
+        props.setGroupValues('subgroup', null);
     };
 
     const groupNameChangeHandler = (event, type) => {
@@ -48,9 +53,7 @@ const ModalContent = props => {
 
     const confirmHandler = () => {
         if (props.classifierName.length > 0) {
-            const newGroup = {...props.newGroup};
-            newGroup.id = props.group.id;
-            props.editGroup({...newGroup}, props.group.id);
+            props.editGroup({...props.newGroup}, props.group.id);
         } else {
             setError('Անվանման դաշտը չպետք է դատարկ լինի')
         }
@@ -62,17 +65,16 @@ const ModalContent = props => {
         props.addSubgroupAction(id)
     };
 
-    const onAddGroup = (event, id) => {
+    const onAddGroup = (event) => {
         event.stopPropagation();
-        props.addGroupAction(id)
+        props.addGroupAction()
     }
 
     const onEditSubgroup =  async (event, id) => {
         event.stopPropagation();
-        await props.getActionById("get", "subgroup", {path: "Group/SubGroup", id: props.catId, param: {id: id}}, id)
+        await props.getSubgroup(id, props.catId);
         props.editSubgroupAction();
     };
-
 
     const deleteHandler = (event, type, param, id = null) => {
         event.stopPropagation();
@@ -83,10 +85,11 @@ const ModalContent = props => {
         }
     };
 
-    const moveHandler = async (event, id) => {
+    const moveHandler = (event, id) => {
         event.stopPropagation();
-        await props.getActionById("get", "subgroup", {path: "Group/SubGroup", id: props.catId, param: {id: id}}, id)
+
         props.setGroupValues('moveElement', id);
+        props.getSubgroup(id);
     };
 
     const cancelMoving = event => {
@@ -132,57 +135,126 @@ const ModalContent = props => {
     };
 
     const backPageHandler = () => {
+        props.setGroupValues(
+            "newGroup", {
+                title_am: '',
+                title_ru: '',
+                title_en: '',
+                required_group: false,
+                group_type: '1'
+            }
+        );
         props.classifierOpenHandler(props.group.id)
     };
 
+    const deleteModalNameRender = (subgroup, group) => {
+        if (subgroup) {
+            return subgroup[`name_${cookie.get('language') || "am"}`]
+        }
+        return group[`title_${cookie.get('language') || "am"}`]
+    }
+
     return (
         <div className={classes.main}>
-            <DeleteContent
-                delete={props.delete}
-                group={props.group}
-                subgroup={props.subgroup}
+            {
+                props.delete ?
+                    <Backdrop
+                        className={classes.backdrop}
+                        // Methods
+                        onClick={deleteModalCloseHandler}
+                    />
+                    :
+                    null
+            }
+            <DeleteModal
+                open={props.delete}
+                groupName={deleteModalNameRender(props.subgroup, props.group)}
+                data={props.subgroup}
+                alertText={'Դուք չեք կարող ջնջել տվյալ խումբը, քանի որ այն պարունակում է իրեն կից ապրանքատեսականի'}
+                information={'Եթե տվյալ խումբը պարունակում է ենթախմբեր, ապա ջնջելով այն կջնջվեն նաև իր բոլոր ենթախմբերը․'}
+                question={'Դուք իսկապե՞ս ցանկանում եք ջնջել տվյալ խումբը'}
+                cancelButtonName={'Ոչ'}
+                confirmButtonName={'Այո'}
+                status={props.group ? props.group.required_group : false}
                 // Methods
-                deleteModalCloseHandler={deleteModalCloseHandler}
-                deleteModalConfirmHandler={deleteModalConfirmHandler}
+                closeHandler={deleteModalCloseHandler}
+                deleteHandler={deleteModalConfirmHandler}
             />
             <HeaderContent
                 // Methods
                 handleClose={props.handleClose}
                 backPageHandler={backPageHandler}
             />
-            <SectionContent
-                error={error}
-                group={props.group}
-                groupId={props.groupId}
-                catId={props.catId}
-                newGroup={props.newGroup}
-                newSubgroup={props.newSubgroup}
-                classifierName={props.classifierName}
-                own_select={props.own_select}
-                own_subgroups={props.own_subgroups}
-                own_move={props.own_move}
-                search={props.search}
-                edit={props.edit}
-                add={props.add}
-                subgroupName={props.subgroupName}
-                // Methods
-                groupNameChangeHandler={groupNameChangeHandler}
-                changePositionStatus={props.changePositionStatus}
-                addSubgroup={props.addSubgroup}
-                setGroupValues={props.setGroupValues}
-                editSubgroup={props.editSubgroup}
-                changeSubgroupName={props.changeSubgroupName}
-                toggleMovingStatus={toggleMovingStatus}
-                moveHandler={moveHandler}
-                onEditSubgroup={onEditSubgroup}
-                onAddSubgroup={onAddSubgroup}
-                onAddGroup={onAddGroup}
-                deleteHandler={deleteHandler}
-                searchChangeHandler={searchChangeHandler}
-                selectTreeItem={props.selectTreeItem}
-                selectTreeGroupItem={props.selectTreeGroupItem}
-                cancelEditing={props.cancelEditing}
-            />
+            <section>
+                <div className={classes.content}>
+                    <div className={classes.nameWindow}>
+                        <CustomInput
+                            type={'text'}
+                            id={'group-title_am'}
+                            disabled={props.group && props.group.id === 0}
+                            classNameInput={error ? `${classes.nameInput} ${classes.errorField}` : classes.nameInput}
+                            classNameLabel={classes.nameLabel}
+                            name={'title'}
+                            placeholder={'Դասակագիչի անվանում'}
+                            value={props.classifierName}
+                            // Methods
+                            onChange={event => groupNameChangeHandler(event, 'title')}
+                        />
+                        <CustomCheckbox
+                            id={'required_group'}
+                            label={'Պարտադիր'}
+                            disabled={props.group && props.group.id === 0}
+                            labelStyle={classes.labelStyle}
+                            checked={props.newGroup.required_group}
+                            status={props.newGroup.required_group}
+                            name={'required_group'}
+                            // Methods
+                            onChange={event => groupNameChangeHandler(event, 'required_group')}
+                        />
+                    </div>
+                    <div className={classes.searchWindow}>
+                        <ModalActions
+                            own_select={props.own_select}
+                            controllerId={props.controllerId}
+                            groupId={props.groupId}
+                            catId={props.catId}
+                            // Methods
+                            changePositionStatus={props.changePositionStatus}
+                            toggleMovingStatus={toggleMovingStatus}
+                            moveHandler={moveHandler}
+                            onEditSubgroup={onEditSubgroup}
+                            onAddSubgroup={onAddSubgroup}
+                            onAddGroup={onAddGroup}
+                            deleteHandler={deleteHandler}
+                        />
+                        <div>
+                            <CustomSearch
+                                drop={false}
+                                withButton={false}
+                                id={'modalSearch'}
+                                type={'search'}
+                                name={'search'}
+                                value={props.search}
+                                placeholder={'Որոնում'}
+                                // Methods
+                                onChange={event => searchChangeHandler(event.target.name, event.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <BodyContent
+                        data={props.own_subgroups}
+                        search={props.search}
+                        group={props.group}
+                        groupId={props.groupId}
+                        own_move={props.own_move}
+                        own_select={props.own_select}
+                        type={'edit'}
+                        // Methods
+                        selectTreeItem={props.selectTreeItem}
+                        selectTreeGroupItem={props.selectTreeGroupItem}
+                    />
+                </div>
+            </section>
             <FooterContent
                 group={props.group}
                 // Methods
