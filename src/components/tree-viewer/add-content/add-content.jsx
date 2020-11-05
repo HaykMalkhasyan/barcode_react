4794b77218d7ad99483bbcefb01bce3302f82624
HaykMalkhasyan <hayko.malxasyan@mail.ru>
2020-11-05 18:46:18 +0400

    fix add action

--- src/components/tree-viewer/add-content/add-content.jsx
+++ src/components/tree-viewer/add-content/add-content.jsx
@@ -6,42 +6,55 @@ import Icons from "../../Icons/icons";
 
 const AddContent = props => {
 
+    const submitHandler = event => {
+        event.preventDefault();
+        event.stopPropagation();
+        const newSubgroup = {...props.newSubgroup};
+        newSubgroup.name = props.subgroupName;
+        newSubgroup.parent_id = props.add;
+        newSubgroup.cat_id = props.catId;
+        props.addSubgroup(newSubgroup, props.node, props.subLevel);
+    }
+
     return (
-        <div className={`${classes.addWindow} ${props.type === "first" ? classes.first : ''}`}>
-            <div>
-                <CustomInput
-                    autoFocus={true}
-                    classNameInput={classes.addInput}
-                    classNameLabel={classes.addLabel}
-                    label={<Icons type={'tree-arrow-right-empty'}/>}
-                    placeholder={'Ենթախմբի անվանում'}
-                    value={props.subgroupName}
-                    name={'subgroupName'}
-                    // Methods
-                    onChange={event => {
-                        props.changeSubgroupName(event.target.name, event.target.value)
-                    }}
-                />
+        <form
+            className={classes.form}
+            // Methods
+            onSubmit={event => {
+                submitHandler(event)
+            }}
+        >
+            <div className={`${classes.addWindow} ${props.type === "first" ? classes.first : ''}`}>
+                <div>
+                    <CustomInput
+                        autoFocus={true}
+                        classNameInput={classes.addInput}
+                        classNameLabel={classes.addLabel}
+                        label={<Icons type={'tree-arrow-right-empty'}/>}
+                        placeholder={'Ենթախմբի անվանում'}
+                        value={props.subgroupName}
+                        name={'subgroupName'}
+                        // Methods
+                        onChange={event => {
+                            props.changeSubgroupName(event.target.name, event.target.value)
+                        }}
+                    />
+                </div>
+                <div>
+                    <CoupleButtons
+                        type={"add"}
+                        // Methods
+                        checkSuccess={event => {
+                            submitHandler(event)
+                        }}
+                        checkClose={event => {
+                            event.stopPropagation()
+                            props.cancelEditing(props.node, props.subLevel)
+                        }}
+                    />
+                </div>
             </div>
-            <div>
-                <CoupleButtons
-                    type={"add"}
-                    // Methods
-                    checkSuccess={event => {
-                        const newSubgroup = {...props.newSubgroup};
-                        newSubgroup.name = props.subgroupName;
-                        newSubgroup.parent_id = props.add;
-                        newSubgroup.cat_id = props.catId;
-                        props.addSubgroup(newSubgroup, props.node, props.subLevel);
-                        event.stopPropagation()
-                    }}
-                    checkClose={event => {
-                        event.stopPropagation()
-                        props.cancelEditing(props.node, props.subLevel)
-                    }}
-                />
-            </div>
-        </div>
+        </form>
     )
 }
 
