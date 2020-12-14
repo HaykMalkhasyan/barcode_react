7bed2c2e8ab83aad0755eb98c29c4beb52d515d4
HaykMalkhasyan <hayko.malxasyan@mail.ru>
2020-12-14 19:01:00 +0400

    chench filters window hide/show button

--- src/containers/pages/products/filters/searchWindow/customSearchWindow/customSearchWindow.jsx
+++ src/containers/pages/products/filters/searchWindow/customSearchWindow/customSearchWindow.jsx
@@ -2,11 +2,27 @@ import React from 'react'
 import classes from './customSearchWindow.module.css'
 import CustomHeader from "../../../../../../components/UI/customHeader/customHeader";
 import CustomSearch from "../../../../../../components/customSearch/customSearch";
+import CustomButton from "../../../../../../components/UI/button/customButton/custom-button";
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
 const CustomSearchWindow = props => {
 
     return (
         <div className={classes.searchContainer}>
+            <CustomButton
+                className={classes.screenButton}
+                children={
+                    props.screen ?
+                        <ArrowLeftIcon className={classes.screenIcon}/>
+                        :
+                        <ArrowRightIcon className={classes.screenIcon}/>
+                }
+                // EVENTS
+                onClick={() => {
+                    props.setProductValues("screen", !props.screen)
+                }}
+            />
             <CustomHeader
                 name={'Որոնել'}
             />
